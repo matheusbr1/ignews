58cbcf47c91f27399ef2d482c6bb8b3a4c8742b7
Matheus Baron <matheusbaron10@gmail.com>
2021-06-06 12:51:38 -0300

    Feat: social authentication added

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,16 @@
 import { FaGithub } from 'react-icons/fa'
 import styles from './styles.module.scss'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export function SignInButton() {
+  const [ session ] = useSession()
 
-  const isUserLoggedIn = false 
-
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type='button' 
       className={styles.signInButton} 
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
       Matheus Baron
@@ -19,6 +20,7 @@ export function SignInButton() {
     <button 
       type='button' 
       className={styles.signInButton} 
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with GitHub
