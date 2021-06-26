28a4cd66ed87a4d0e32a63071288632209c82817
Matheus Baron <matheusbaron10@gmail.com>
2021-06-26 11:34:09 -0300

    Feat: prismic API consumed

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,8 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { getPrismicCLient } from '../../services/prismic'
 import styles from './styles.module.scss'
+import Prismic from '@prismicio/client'
 
 export default function Posts() {
   return (
@@ -31,4 +34,23 @@ export default function Posts() {
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps =  async () => {
+  const prismic = getPrismicCLient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.content'],
+    pageSize: 100
+  })
+
+  console.log(response)
+
+  return {
+    props : {
+
+    }
+  }
 }
